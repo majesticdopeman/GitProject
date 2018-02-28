79298b0c7162f3c375e6c30fa0e582d48a7dcfbb
majesticdopeman <36930184+majesticdopeman@users.noreply.github.com>
2018-02-28 11:31:58 -0600

    Update Bomb.py

--- Bomb.py
+++ Bomb.py
@@ -4,9 +4,9 @@ class Bomb(object):
     """This creates a bomb object"""
 
     def __init__(self, center:Point, blastRadius = 200):
-        self.center = center
+        self.center = center #this is the center
         self.radius = blastRadius
-        self.color = "red"
+        self.color = "blue"
         self.blast = Circle(self.center, self.radius)
 
     def drawBomb(self, win):
